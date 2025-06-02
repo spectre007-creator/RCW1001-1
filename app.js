076a6bb11661ae7d88e8e38f4586b5f94a64ea7e
spectre007-creator <youssef.abdelouhab2023@gmail.com>
2025-06-02 13:32:51 -0400

    delete the files
    delete the files test: app

--- app.js
+++
@@ -1,6 +0,0 @@
-
-// Afficher un message apres 3 seconds
-
-setTimeout(()=>{
-    console.log("Bonjour Teccart, apres un delai de 3 secends");
-},3000)
--- app.py
+++
@@ -1,7 +0,0 @@
-# affichage les numero de 1 - 10
-
-for i in range(1,11):
-    print(i)
-
-while(i<5):
-    print(i)    
